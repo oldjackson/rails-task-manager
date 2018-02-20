02f612e7a15347afa4b6ea0a6251f4a6bda12f66
Lorenzo Fermi <lorenzofermi@gmail.com>
2018-02-20 16:47:47 +0000

    Delete working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,13 +26,18 @@ class TasksController < ApplicationController
   end
 
   def update
-    binding.pry
     task = Task.find(params[:id])
     if task.update(task_params)
       redirect_to task_path(task)
     else
       render :edit
     end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.delete
+    redirect_to tasks_path
   end
 
   private
